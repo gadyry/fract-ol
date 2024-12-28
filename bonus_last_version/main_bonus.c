0173e6fe3f8aa149bf74d639c53c04c8cc6f75c3
Abdelhak El Gadyry <ael-gady@e3r1p5.1337.ma>
2024-12-28 22:00:49 +0100

    push_last_v

---
+++ bonus_last_version/main_bonus.c
@@ -0,0 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_bonus.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-gady <ael-gady@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/27 20:10:31 by ael-gady          #+#    #+#             */
+/*   Updated: 2024/12/28 21:49:11 by ael-gady         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fractol_bonus.h"
+
+static void	init_var(int *pt, int *s, int *n_d, int *d)
+{
+	*pt = 0;
+	*s = 0;
+	*n_d = 0;
+	*d = 0;
+}
+
+int	ft_check_arg(char *arg)// check ".5" - "++5"
+{
+	int	i;
+	int	pt;
+	int	sign;
+	int	non_digit;
+	int	digit;
+
+	i = -1;
+	init_var(&pt, &sign, &non_digit, &digit);
+	while (arg[++i])
+	{
+		if ((arg[i] == '+' || arg[i] == '-') && i != 0)
+			return (0);
+		if (arg[i] == '+' || arg[i] == '-')
+			sign++;
+		else if (arg[i] == '.')
+			pt++;
+		else if (arg[i] < '0' || arg[i] > '9')
+			non_digit++;
+		else
+			digit++;
+	}
+	if (arg[i - 1] == '.' || pt > 1 || sign > 1 || \
+		non_digit > 0 || digit < 1)
+		return (0);
+	return (1);
+}
+
+void	create_fractol(t_my_fractol *f, char *str)
+{
+	if (!ft_strcmp(str, "mandelbrot"))
+		create_fract_mandelbrot(f);
+	else if (!ft_strcmp(str, "julia"))
+		create_fract_julia(f);
+	// else
+	// 	create_fract_tricorn(f);
+	managing_events(f);
+}
+
+void	preparing_fractol(t_my_fractol *fr)
+{
+	fr->mlx_ptr = NULL;
+	fr->mlx_win = NULL;
+	fr->img.img_ptr = NULL;
+	fr->mlx_ptr = mlx_init();
+	if (fr->mlx_ptr == NULL)
+		deal_with_error("Problem in connection!\n", fr);
+	fr->mlx_win = mlx_new_window(fr->mlx_ptr, WIDTH, HEIGHT, fr->name);
+	if (fr->mlx_win == NULL)
+		deal_with_error("Problem creating the window!\n", fr);
+	fr->img.img_ptr = mlx_new_image(fr->mlx_ptr, WIDTH, HEIGHT);
+	if (fr->img.img_ptr == NULL)
+		deal_with_error("Problem creating the image!\n", fr);
+	fr->img.pixel_data = mlx_get_data_addr(fr->img.img_ptr, \
+	&fr->img.bits_per_pixel, &fr->img.line_length, &fr->img.endian);
+	if (fr->img.pixel_data == NULL) // not  important  to handle it 1
+		deal_with_error("Problem accessing image data!\n", fr);
+	set_plan(fr);
+}
+
+int	main(int ac, char *av[])
+{
+	t_my_fractol	fractol;
+
+	if (ac == 2 && (!ft_strcmp(av[1], "mandelbrot") \
+		|| !ft_strcmp(av[1], "tricorn")))
+	{
+		fractol.name = av[1];
+		preparing_fractol(&fractol);
+		create_fractol(&fractol, fractol.name);
+		mlx_loop(fractol.mlx_ptr);
+	}
+	else if (ac == 4 && !ft_strcmp(av[1], "julia")
+		&& ft_check_arg(av[2]) && ft_check_arg(av[3]))
+	{
+		fractol.name = "julia";
+		fractol.c_julia.real = ft_atod(av[2]);
+		fractol.c_julia.img = ft_atod(av[3]);
+		preparing_fractol(&fractol);
+		create_fractol(&fractol, fractol.name);
+		mlx_loop(fractol.mlx_ptr);
+	}
+	else
+		ft_put_error(ERROR_0 ERROR_1 ERROR_2, 2);
+	return (0);
+}
---
+++ bonus_last_version/mandelbrot_bonus.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mandelbrot_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-gady <ael-gady@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/27 20:39:16 by ael-gady          #+#    #+#             */
+/*   Updated: 2024/12/28 21:54:01 by ael-gady         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fractol_bonus.h"
+
+void	handle_data_pixel_mand(t_my_fractol *f, int x, int y)
+{
+	t_complex_nbr	z;
+	t_complex_nbr	c;
+
+	z.real = 0.0;
+	z.img = 0.0;
+	f->nbr_iter = 0;
+	c.real = map_pixel_to_coordinate(x, f->x_start, f->x_end, WIDTH);
+	c.img = map_pixel_to_coordinate(y, f->y_start, f->x_end, HEIGHT);
+	while (f->nbr_iter < MAX_ITER)
+	{
+		z = sum_complex(pow2_complex(z), c);
+		if ((z.real * z.real) + (z.img * z.img) > 4)
+		{
+			put_color(f, x, y, f->nbr_iter);
+			return ;
+		}
+		f->nbr_iter++;
+	}
+	put_postition_pixel(f, x, y, BLACK_COLOR);
+}
+
+void	create_fract_mandelbrot(t_my_fractol *f)
+{
+	int x;
+	int y;
+
+	y = 0;
+	while (y < WIDTH)
+	{
+		x = 0;
+		while (x < HEIGHT)
+		{
+			handle_data_pixel_mand(f, x, y);
+			x++;
+		}
+		y++;
+	}
+	mlx_put_image_to_window(f->mlx_ptr, f->mlx_win, f->img.img_ptr, 0, 0);
+}
